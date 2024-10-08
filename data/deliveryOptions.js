355a2c5913995089899911abfcd8b0debc54c541
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-10-08 13:29:06 -0400

    save data and generate html for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -12,3 +12,14 @@ export const deliveryOptions = [{
     priceCents: 999
 }]
 
+export function getDeliveryOption(deliveryOptionId){
+    let deliveryOption;
+
+            deliveryOptions.forEach((option) => {
+                if(option.id === deliveryOptionId){
+                    deliveryOption = option;
+                }
+            })
+
+            return deliveryOption || deliveryOptions[0]
+}
